bc77f286b2b2a4b4e6644e1269771129092da77a
noname2048 <sungwook.csw@gmail.com>
2022-05-07 23:08:17 +0900

    feat: get basic of images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import Post from "./Post";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
-  return "Hello world";
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + "/post/all")
+      .then((response) => {
+        const json = response.json();
+        console.log(json);
+        if (response.ok) {
+          return json;
+        }
+        throw response;
+      })
+      .then((data) => {
+        setPosts(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }, []);
+
+  return (
+    <div className="app_posts">
+      {posts.map((post) => (
+        <Post post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
